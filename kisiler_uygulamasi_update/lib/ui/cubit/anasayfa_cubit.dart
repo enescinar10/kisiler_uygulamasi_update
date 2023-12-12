f2dbd31f4a6aa3b4e8c309942998037ce3e09bb9
Enes Çınar <enescinar2091@gmail.com>
2023-12-12 21:58:28 +0300

    kisiler_uygulamasi_update
    Flutter ile kisiler uygulamasi

---
+++ kisiler_uygulamasi_update/lib/ui/cubit/anasayfa_cubit.dart
@@ -0,0 +1,49 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kisiler_uygulamasi/data/entity/kisiler.dart';
+import 'package:kisiler_uygulamasi/data/repo/kisiler_dao_repository.dart';
+
+class AnasayfaCubit extends Cubit<List<Kisiler>> {
+  AnasayfaCubit():super(<Kisiler>[]);
+  var krepo = KisilerDaoRepository();
+  var collectionKisiler = FirebaseFirestore.instance.collection("Kisiler");
+
+  Future<void> kisileriYukle() async {
+    collectionKisiler.snapshots().listen((value) {
+      var kisilerListesi = <Kisiler>[];
+      var documents = value.docs; // tüm dökmanları verir tüm kayıtları verir
+      for(var document in documents){
+        var data = document.data();
+        var key = document.id;
+        var kisi = Kisiler.fromJson(data, key);
+        kisilerListesi.add(kisi);
+      }
+
+      emit(kisilerListesi);
+    });
+  }
+
+  Future<void> ara(String aramaKelimesi) async {
+    //var sorgu = collectionKisiler.orderBy("kisi_ad",descending: true);
+    var sorgu = collectionKisiler.limit(2);
+    sorgu.snapshots().listen((value) {
+      var kisilerListesi = <Kisiler>[];
+      var documents = value.docs; // tüm dökmanları verir tüm kayıtları verir
+      for(var document in documents){
+        var data = document.data();
+        var key = document.id;
+        var kisi = Kisiler.fromJson(data, key);
+        if(kisi.kisi_ad.toLowerCase().contains(aramaKelimesi.toLowerCase())){
+          kisilerListesi.add(kisi);
+        }
+        kisilerListesi.add(kisi);
+      }
+
+      emit(kisilerListesi);
+    });
+  }
+
+  Future<void> sil(String kisi_id) async{
+    await krepo.sil(kisi_id);
+  }
+}
